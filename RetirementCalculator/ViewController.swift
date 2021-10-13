8c2f1ba86a0be4cacb13b07e022bf17e7e9821df
Alena Nesterkina <elena.walst@bk.ru>
2021-10-13 11:55:49 +0300

    AppCenter start

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import AppCenterCrashes
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthlyInvestmentTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var retirementAgeTextField: UITextField!
+    @IBOutlet weak var interestAgeTextField: UITextField!
+    @IBOutlet weak var savingsTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateResult(_ sender: UIButton) {
+        Crashes.generateTestCrash()
+    }
+    
 }
 
